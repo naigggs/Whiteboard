f45604d7a409b047080faf889ad0fe0b2f3b0915
Gian Cabigting <giancabigting1026@gmail.com>
2022-11-11 10:45:50 +0800

    tT

--- Whiteboard/settings/base.py
+++ Whiteboard/settings/base.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-h%!(=zqy907&(zclez824a0=$-rfxv+_er72suz&%1^l^%&q%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
